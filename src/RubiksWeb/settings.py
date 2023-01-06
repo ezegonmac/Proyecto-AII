951e055472065d8954d946d0d95e2fb4b848ac34
ezegonmac <ezegonmac01@gmail.com>
2023-01-06 21:53:57 +0100

    Add basic style for login view

--- src/RubiksWeb/settings.py
+++ src/RubiksWeb/settings.py
@@ -130,6 +130,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/home'
 
 # Console log emails reset passwords
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
