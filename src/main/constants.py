8eb526935138e8c33bbea875444fe97e5ae2d909
ezegonmac <ezegonmac01@gmail.com>
2023-01-08 15:49:39 +0100

    Fix index not working in production. Replace relative to absolute path

--- src/main/constants.py
+++ src/main/constants.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SCRAPING
 
@@ -6,7 +9,7 @@ NUM_PAGES_TO_SCRAPE = 47 # max 48 ALL pages
 
 # INDEX
 
-INDEX_FOLDER = "Index"
+INDEX_FOLDER = BASE_DIR + "/Index"
 INDEX_ITEMS = "items_index"
 INDEX_BRANDS = "brands_index"
 INDEX_TYPES = "types_index"
@@ -14,7 +17,6 @@ INDEX_MAGNETS = "magnets_index"
 INDEX_EXTERIOR_FINISH = "exterior_finishes_index"
 INDEX_PLASTIC_COLOR = "plastic_colors_index"
 INDEX_INTERIOR_PLASTIC_COLOR = "interior_plastic_colors_index"
-
 
 # RECOMMENDATIONS
 
